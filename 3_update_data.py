169543b605a7903c3b031d28f9107b95b8fd5f3f
James Vilela-Slater <hamzvilelas@gmail.com>
2024-12-02 17:13:15 +0000

    Update 3_update_data.py
    Learnt a range of skills to update table(s)
    > Update a row in a specific table depending on its 'ID'
    > Update an item in all rows in a particular table.
    > print verification statements for updates made, on all rows and specific ideas.

--- 3_update_data.py
+++ 3_update_data.py
@@ -3,27 +3,70 @@ import sqlite3
 
 db_name = 'my_database.db'
 
-# - To  bind arguments to the `UPDATE` statement, use `?` for each argument:
-# UPDATE table_name SET column1=?, column2=? WHERE id = ?
+''' - To  bind arguments to the `UPDATE` statement, use `?` for each argument:
+*update_datement* = UPDATE table_name SET column1=?, column2=? WHERE id = ?
+'''
+# construct an update statement:
+table = 'tasks'
+update_statement = f'UPDATE {table} SET priority = ?, status_id = ? WHERE id = ?'
+date_update_state = f'UPDATE {table} SET end_date = ?'
 
-# construct an update statement: 
-update_statement = 'UPDATE tasks SET priority = ?, status_id = ? WHERE id = ?'
 
-def update_data(conn, sql, arg1, arg2, id_):
+def update_data_test(conn, sql, arg1, arg2, ID):
     cursor = conn.cursor()
     try:
-        # execute the UPDATE statement with the priority and id. 
-        cursor.execute(sql, (arg1, arg2, id_))
+        # execute the UPDATE statement with the priority and id.
+        cursor.execute(f"SELECT * FROM {table} WHERE id = ?", (ID,))
+        if cursor.fetchone() is None:
+            print(f"No task found with id: `{ID}`.")
+            return
+        
+        # Execute the UPDATE statement
+        cursor.execute(sql, (arg1, arg2, ID))
         conn.commit()
+        if cursor.rowcount == 0:
+            print(f"No '{table}' table found with id `{ID}`.")
+        else:
+            print(f"'{table}' table found with id `{ID}`.")
+
+        # verify the update on a specific row: 
+        cursor.execute('SELECT id, priority, status_id FROM tasks WHERE id = ?', (ID,))
+        row = cursor.fetchone() # Gets all rows from the result of the query
+        if row: 
+            print("Verifying the update:")
+            print(row)
+        else:
+            print(f"No '{table}' table found with id `{ID}`.")
+
     except sqlite3.Error as e:
         print(f"An error occured in the UPDATE statement: {e}") 
+
+
+# With no ID specified the update is applied to ALL rows
+def update_date(conn, sql, arg1): 
+    cursor = conn.cursor()
+    try:
+        # Execute the UPDATE statement
+        cursor.execute(sql, (arg1,) )
+        conn.commit()
+
+        # print a query result of the updates made on ALL rows: 
+        cursor.execute('SELECT id, name, end_date FROM tasks')
+        rows = cursor.fetchall() # Gets all rows from the result of the query
+        print("Verifying the date update:")
+        for row in rows:
+            print(row)
+    
+    except sqlite3.Error as e:
+        print(f"An error occured in the 'date' UPDATE statement: {e}") 
+
 
 def main():
     try:
         with sqlite3.connect(db_name) as conn:
             # UPDATE one field of one row in the table!
-            update_data(conn, update_statement)
-
+            update_data_test(conn, update_statement, 58, 52, 2)
+            update_date(conn, date_update_state, '2004-03-10')
     except sqlite3.Error as e:
         print(e)
 
